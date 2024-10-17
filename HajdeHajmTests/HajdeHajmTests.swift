2658d59568f3c48c905278cca0c75722a612f2df
kujtimsaliu <kujtimsaliu011@gmail.com>
2024-10-17 20:49:39 +0200

    Initial Commit

---
+++ HajdeHajmTests/HajdeHajmTests.swift
@@ -0,0 +1,17 @@
+//
+//  HajdeHajmTests.swift
+//  HajdeHajmTests
+//
+//  Created by Kujtim Saliu on 17.10.24.
+//
+
+import Testing
+@testable import HajdeHajm
+
+struct HajdeHajmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
